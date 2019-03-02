5ee5b816e1ebc66a3a6f9d09b6f987c265863467
Eric Carlson <eric.paul.carlson@gmail.com>
2019-03-02 14:35:12 -0800

    added post, put, and delete for gifts and lists

--- gifts/router.js
+++ gifts/router.js
@@ -27,4 +27,87 @@ router.get('/:userId', (req, res) => {
         });
 });
 
+router.post('/gifts', jsonParser, (req, res) => {
+    const requiredFields = ['name', 'price', 'holiday', 'recipient', 'description', 'link', 'image']
+    let newGift = {
+        name: req.body.name,
+        price: req.body.price,
+        holiday: req.body.holiday,
+        recipient: req.body.recipient,
+        description: req.body.description,
+        link: req.body.link,
+        image: req.body.image
+    };
+
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message)
+        };
+    };
+
+    Gift
+        .create(newGift)
+        .then(() => {
+            res.status(201).json(newGift)
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Something went terribly wrong' })
+        });
+});
+
+router.post('/lists/:userId', jsonParser, (req, res) => {
+    const requiredFields = 'title';
+    const user = req.params.userId;
+    console.log('hello')
+
+    let newList = {
+        title: req.body.title,
+        gifts: []
+    };
+
+    if (!(requiredFields in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message)
+    };
+
+    List
+        .create(newList)
+        .then((list) => {
+            newList = list;
+            return Account.findOne({ user: user });
+        })
+        .then((account) => {
+            account.lists.push(newList);
+            return account.save(function () { });
+        })
+        .then((savedAccount) => {
+            res.status(201).json(newList)
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'something went horribly wrong' });
+        })
+});
+
+router.delete('/lists', jsonParser, (req, res) => {
+    let toBeDeleted = req.body;
+
+    List
+        .remove({ _id: { $in: toBeDeleted }, }, () => { res.status(204).json({ message: "List deletion successful" }).end() })
+        .catch(err => { res.status(500).json({ error: "something went terribly wrong deleting a list" }) });
+});
+
+router.put('/lists', jsonParser, (req, res) => {
+    let updatedGifts = { gifts: req.body.gifts }
+
+    List
+        .findOneAndUpdate({ _id: req.body._id }, { $set: updatedGifts })
+        .then(response => { res.status(200).json({message: "updated gift list"}) })
+        .catch(err => res.status(500).json({message: "Something went horribly wrong updating the gift list"}));
+});
 module.exports = { router };
