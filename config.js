016059283e5883c3685111f1f7a01bb944b8c17b
Eric Carlson <eric.paul.carlson@gmail.com>
2019-03-14 00:21:11 -0700

    remote deploy

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
-//exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://user1:password1@ds341825.mlab.com:41825/giftlist';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/giftList';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://user1:password1@ds341825.mlab.com:41825/giftlist';
+//exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/giftList';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/testGiftList';
 
 exports.PORT = process.env.PORT || 8080;
